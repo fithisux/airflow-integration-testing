ef6fd9e99cf02cc57b3e1631b383ac97e896c8f1
Vasileios Anagnostopoulos <fithis2001@gmail.com>
2023-05-12 18:22:14 +0300

    feat: add time out

--- tests/integration/dag_trigger.py
+++ tests/integration/dag_trigger.py
@@ -6,6 +6,7 @@ from airflow_client.client.api import dag_api, dag_run_api
 from airflow_client.client.model.dag import DAG
 from airflow_client.client.model.dag_run import DAGRun
 
+RETRIES = 10
 
 def run_dag(dag_id: str):
     configuration = airflow_client.client.Configuration(host="http://localhost:8080/api/v1", username="airflow", password="airflow")
@@ -37,13 +38,16 @@ def run_dag(dag_id: str):
 
         print("[blue]Status of a DAG run")
         try:
-            is_not_success = True
-            while is_not_success:
+            is_not_success = RETRIES
+            while True:
                 api_response = dag_run_api_instance.get_dag_run(dag_id, dag_run_id)
                 print(api_response.state)
                 if str(api_response.state) == "success":
-                    is_not_success = False
+                    break
                 else:
+                    is_not_success = is_not_success - 1
+                    if is_not_success <= 0:
+                        raise Exception('Could not finish in time')
                     time.sleep(1)
 
         except airflow_client.client.exceptions.OpenApiException as e:
